580480b951e3d49900f68596f3ea18165fa03440
Knight0042 <dillon.barnes004@gmail.com>
2026-03-23 19:24:16 -0500

    add publishing the detection information as a ros message/topic

--- vision_testing/vision_pkg/vision_pkg/vision_node.py
+++ vision_testing/vision_pkg/vision_pkg/vision_node.py
@@ -7,6 +7,9 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from vision_msgs.msg import Detection2DArray, Detection2D, ObjectHypothesisWithPose
+from geometry_msgs.msg import Pose2D
+
 
 class VisionNode(Node):
     def __init__(self):
@@ -40,6 +43,7 @@ class VisionNode(Node):
         self.timer = self.create_timer(0.05, self.timer_callback)
         self.bridge = CvBridge()
         self.image_pub = self.create_publisher(Image, '/vision/image', 10) # create a publisher for images for viewing
+        self.detections_pub = self.create_publisher(Detection2DArray, '/vision/detections', 10) # create a publisher for detection data
     def timer_callback(self):
     
         # key = cv2.waitKey(1) & 0xFF
@@ -54,23 +58,46 @@ class VisionNode(Node):
             return
 
         results = self.model(img, stream=True)
+
+        detection_array_msg = Detection2DArray()
+        detection_array_msg.header.stamp = self.get_clock().now().to_msg()
+        detection_array_msg.header.frame_id = "camera_frame"
+
         detected_objects = []
         for r in results:
             boxes = r.boxes
             for box in boxes:
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                x1, y1, x2, y2 = box.xyxy[0].tolist() # the tolist helps avoid weird issues with types
+                x1_i, y1_i, x2_i, y2_i = int(x1), int(y1), int(x2), int(y2)
                 # cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 255), 3)
                 w, h = x2 - x1, y2 - y1
-                cvzone.cornerRect(img, (x1, y1, w, h))
+                cvzone.cornerRect(img, (x1_i, y1_i, int(w), int(h)))
                 # Confidence
                 conf = math.ceil((box.conf[0] * 100)) / 100
                 # Class Name
                 cls = int(box.cls[0])
                 if conf > 0.3:
+                    detection_msg = Detection2D() # contains the data for 1 object
+
+                    detection_msg.header = detection_array_msg.header # match the detection array
+
+                    # Store the data for the bounding box
+                    detection_msg.bbox.center.position.x = (x1 + x2) / 2.0 # Find center point x and y
+                    detection_msg.bbox.center.position.y = (y1 + y2) / 2.0
+                    detection_msg.bbox.size_x = w
+                    detection_msg.bbox.size_y = h # may need to make these floats
+
+                    # Create a hypothesis which has data on what object we believe we have detected and the confidence
+                    hypothesis = ObjectHypothesisWithPose()
+                    hypothesis.hypothesis.class_id = self.classNames[cls]
+                    hypothesis.hypothesis.score = float(conf)
+
+                    detection_msg.results.append(hypothesis)
+                    detection_array_msg.detections.append(detection_msg)
+
                     detected_objects.append(self.classNames[cls])
-                cvzone.putTextRect(img, f' {self.classNames[cls]} {conf}', (max(0, x1), max(30, y1)), scale=0.8,
-                                thickness=1)
+                cvzone.putTextRect(img, f' {self.classNames[cls]} {conf}', (max(0, x1_i), max(30, y1_i)), scale=0.8,
+                                thickness=1) # this is just to help visualize
         # Get rid of repeated detections of the same card
         detected_objects = list(set(detected_objects))
         self.get_logger().info(f"Detected objects: {detected_objects}")
@@ -96,6 +123,12 @@ class VisionNode(Node):
         ros_img.header.stamp = self.get_clock().now().to_msg()
         ros_img.header.frame_id = "camera_frame"
         self.image_pub.publish(ros_img) # publish the image as a topic
+
+        
+
+
+
+        self.detections_pub.publish(detection_array_msg)
 
 
 def main(args=None):
